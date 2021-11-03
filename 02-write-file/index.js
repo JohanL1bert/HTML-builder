6e0d78e866d402e1104f510ce58b3aeeb203aa4b
Johan <grimmerw968@gmail.com>
2021-11-03 21:59:02 +0200

    feat: add solution to files-in-folder

---
+++ 02-write-file/index.js
@@ -0,0 +1,10 @@
+/* const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+const readLine = require('readline').createInterface({
+  input: stdin,
+  output: stdout
+});
+
+const pathToFolder = (__dirname);
+ */
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const pathToFolder = path.join(__dirname, '/secret-folder');
+
+fs.readdir(pathToFolder, {withFileTypes: true}, (err, files) => {
+  if(err){
+    stdout.write(err + '\n');
+  }
+
+  files.forEach(el => {
+    if (el.isFile()) {
+      fs.stat(path.join(pathToFolder, el.name), (err, stats) => {
+        if (err) stdout.write(err + '\n');
+        const fileData = path.parse(el.name);
+        const fileName = fileData.name;
+        const fileExt = fileData.ext;
+        const fileKbytes = Math.ceil(stats.size / Math.pow(1024, 1));
+        const fileBytes = stats.size / 1000;
+        stdout.write(fileName + ' - ' + fileExt.slice(1) + ' : ' + fileKbytes + 'kb' + '  or  ' + fileBytes + 'b' + '\n'); 
+      });
+    }
+  });
+});
