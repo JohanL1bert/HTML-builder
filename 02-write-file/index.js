a2eb6b63c39a73b22d6c9d95ee2f499ac8631e81
Johan <grimmerw968@gmail.com>
2021-11-04 15:26:28 +0200

    feat: add solution for write file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,37 @@
-/* const fs = require('fs');
+const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
-const readLine = require('readline').createInterface({
+const readLineText = require('readline').createInterface({
   input: stdin,
-  output: stdout
+  output: stdout,
 });
 
-const pathToFolder = (__dirname);
- */
+
+readLineText.on('SIGINT', () => {
+  stdout.write('\n');
+  stdout.write('You press CTR + C. Nothing was written to the file. GoodBye');
+  process.exit();
+});
+
+const writeFileAnswer = (data) => {
+  fs.appendFile('newFile.txt', `${data + '\n'}`, (err) => {
+    if(err) throw err;
+  });
+};
+
+const pathToFolder = path.join(__dirname, './newFile.txt'); 
+fs.open(pathToFolder, 'w', (err) => {
+  if (err) throw err;
+  const createQuestion = () => {
+    readLineText.question('Type Something: ', answer => {
+      if (answer.toLowerCase() === 'exit') {
+        stdout.write('GoodBye');
+        readLineText.close();
+      } else {
+        writeFileAnswer(answer);
+        createQuestion();
+      }
+    });
+  };
+  createQuestion();
+});
